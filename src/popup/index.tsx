760ab4e058ac02672ce6cd9946d9748252840c83
zaichu <zaitomo41@gmail.com>
2025-06-11 00:50:48 +0900

    CSPエラー対策

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import RakutenCsvExtensionApp from "./RakutenCsvExtensionApp";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 console.log("楽天証券CSV拡張機能のポップアップが読み込まれました");
 
