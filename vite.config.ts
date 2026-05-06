e4573533999f78d121836aa3041c4fb1358ad930
Tomoya Zaitsu <zaitomo41@gmail.com>
2026-05-07 01:57:08 +0900

    chore: remove crxjs vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json'
 import packageJson from './package.json'
-
-// package.jsonのバージョンをmanifest.jsonに反映
-const manifestWithVersion = {
-  ...manifest,
-  version: packageJson.version
-}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    crx({ manifest: manifestWithVersion })
   ],
   define: {
-    // package.jsonのバージョンをグローバル変数として注入
     __APP_VERSION__: JSON.stringify(packageJson.version)
   },
   build: {
     outDir: 'dist',
     minify: 'esbuild',
     rollupOptions: {
+      input: {
+        popup: 'src/popup/index.html',
+      },
       output: {
         chunkFileNames: 'assets/[name]-[hash].js',
         entryFileNames: 'assets/[name]-[hash].js',
